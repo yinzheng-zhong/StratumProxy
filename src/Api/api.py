a1d635b57ef0e01fbc0d6c5bc79e9077271da55e
zxhaxdr <zxhaxdr@gmail.com>
2021-10-18 15:07:37 +0100

    handle api error

--- src/Api/api.py
+++ src/Api/api.py
@@ -1,4 +1,5 @@
 import requests
+import logging
 
 
 class Api:
@@ -6,22 +7,25 @@ class Api:
         self.algo = algo
         self.coin_lists = coin_lists
 
-
     def get_most_profitable(self):
-        response = requests.get("http://api.zergpool.com:8080/api/currencies").json()
+        try:
+            response = requests.get("http://api.zergpool.com:8080/api/currencies").json()
 
-        max_profit = 0
-        coin_to_mine = ''
+            max_profit = 0
+            coin_to_mine = ''
 
-        for i in self.coin_lists:
-            for j in response.keys():
-                if i in j and response[j]['algo'] == self.algo:
-                    current_estimate = float(response[j]['estimate_current'])
-                    if current_estimate > max_profit:
-                        max_profit = current_estimate
-                        coin_to_mine = i
+            for i in self.coin_lists:
+                for j in response.keys():
+                    if i in j and response[j]['algo'] == self.algo:
+                        current_estimate = float(response[j]['estimate_current'])
+                        if current_estimate > max_profit:
+                            max_profit = current_estimate
+                            coin_to_mine = i
 
-        return coin_to_mine
+            return coin_to_mine
+        except Exception as e:
+            logging.warning(e)
+            return self.coin_lists[0]
 
 
 if __name__ == '__main__':
