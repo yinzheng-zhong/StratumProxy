8e970d05a04e776cb4377ddcb463557c31007577
zxhaxdr <zxhaxdr@gmail.com>
2021-10-18 09:31:14 +0100

    subprocess

--- scrypt_run.py
+++ scrypt_run.py
@@ -3,7 +3,7 @@ import subprocess
 filename = 'main.py script'
 while True:
     """However, you should be careful with the '.wait()'"""
-    p = subprocess.Popen('python ' + filename, shell=True).wait()
+    p = subprocess.Popen('python3 ' + filename, shell=True).wait()
 
     """#if your there is an error from running 'my_python_code_A.py', 
     the while loop will be repeated, 
