f2aed570613203233d315f35553fc81587f6578d
zxhaxdr <zxhaxdr@gmail.com>
2021-10-18 09:36:48 +0100

    subprocess

--- main.py
+++ main.py
@@ -6,10 +6,7 @@ if __name__ == '__main__':
     arg = sys.argv[1]
     print('Mining using ' + arg)
 
-    try:
-        server = StratumServer(arg)
-        server.run()
-    except Exception as e:
-        print(e, 'exception in main')
+    server = StratumServer(arg)
+    server.run()
 
     time.sleep(10)
