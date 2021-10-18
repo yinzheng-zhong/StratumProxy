6a4a7ed7ad68a1ccc001afc956395e50fe8383b0
zxhaxdr <zxhaxdr@gmail.com>
2021-10-18 14:58:05 +0100

    use pass algo to script

--- script_run.py
+++ script_run.py
@@ -1,8 +1,10 @@
 import subprocess
 import time
+import sys
 
+arg = sys.argv[1]
 
-filename = 'main.py scrypt'
+filename = 'main.py ' + arg
 while True:
     proc = subprocess.Popen('python3 ' + filename, shell=True).wait()
 
