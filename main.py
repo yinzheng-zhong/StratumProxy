be8d4d0f990dde61663988cc612cdf894342b932
zxhaxdr <zxhaxdr@gmail.com>
2021-10-18 09:52:03 +0100

    subproc

--- main.py
+++ main.py
@@ -9,4 +9,3 @@ if __name__ == '__main__':
     server = StratumServer(arg)
     server.run()
 
-    time.sleep(10)
--- scrypt_run.py
+++ scrypt_run.py
@@ -1,6 +1,10 @@
 import subprocess
+import time
 
 filename = 'main.py scrypt'
 while True:
     """However, you should be careful with the '.wait()'"""
     p = subprocess.Popen('python3 ' + filename, shell=True).wait()
+    print(p)
+
+    time.sleep(10)
