52c169a37a42eb590c7b998f2266a6c0b81e31b6
zxhaxdr <zxhaxdr@gmail.com>
2021-10-19 09:21:53 +0100

    hot fix remove setblocking(False)

--- src/Proxy/client.py
+++ src/Proxy/client.py
@@ -19,7 +19,7 @@ class Client:
 
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.connect((host, port))
-        s.setblocking(False)
+        #s.setblocking(False)
 
         return s
 
