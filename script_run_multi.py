f06f181ad89c6ed420a880defafb4a0235d75b4d
zxhaxdr <zxhaxdr@gmail.com>
2021-10-19 20:31:34 +0100

    fix subproc

--- script_run_multi.py
+++ script_run_multi.py
@@ -13,18 +13,19 @@ class ProcManager:
         self.maintain()
 
     def add_procs(self):
-        while len(self.proc_list) < self.num_procs:
+        while len(self.proc_list) <= self.num_procs:
             self.proc_list.append(subprocess.Popen('python3 ' + self.filename, shell=True))
 
     def remove_dead_procs(self):
         self.proc_list = [proc for proc in self.proc_list if proc.poll()]
+        print(len(self.proc_list))
 
     def maintain(self):
         while True:
             self.add_procs()
-
-            for proc in self.proc_list:
-                proc.wait()
+            print('*' * 100 + str(len(self.proc_list)) + '*' * 100)
+            self.remove_dead_procs()
+            time.sleep(1)
 
 
 filename = 'main.py ' + sys.argv[1]
