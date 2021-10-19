97b2b3e1c0daffae3cc1bff809cae5a4615e4743
zxhaxdr <zxhaxdr@gmail.com>
2021-10-19 18:02:12 +0100

    multi clients

---
+++ script_run_multi.py
@@ -0,0 +1,19 @@
+import subprocess
+import time
+import sys
+
+arg = sys.argv[1]
+
+filename = 'main.py ' + arg
+
+clients = []
+
+while True:
+    while len(clients) < 100:
+        clients.append(subprocess.Popen('python3 ' + filename, shell=True))
+
+    for proc in clients:
+        if proc.poll() is None:
+            proc = subprocess.Popen('python3 ' + filename, shell=True)
+
+    time.sleep(1)
