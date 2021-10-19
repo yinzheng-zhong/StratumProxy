3a36281b293121fce08354d3aa912901d2e81fad
zxhaxdr <zxhaxdr@gmail.com>
2021-10-19 19:31:22 +0100

    refactor run

--- script_run_multi.py
+++ script_run_multi.py
@@ -3,17 +3,30 @@ import time
 import sys
 
 
+class ProcManager:
+    def __init__(self, filename, num_procs):
+        self.filename = filename
+        self.num_procs = num_procs
+
+        self.proc_list = []
+
+        self.maintain()
+
+    def add_procs(self):
+        self.proc_list = [subprocess.Popen('python3 ' + self.filename, shell=True)] * int(self.num_procs)
+
+    def remove_dead_procs(self):
+        self.proc_list = [proc for proc in self.proc_list if proc.poll()]
+
+    def maintain(self):
+        while True:
+            self.add_procs()
+            self.remove_dead_procs()
+            time.sleep(1)
+
+
 filename = 'main.py ' + sys.argv[1]
+num_processes = int(sys.argv[1])
+pm = ProcManager(filename, num_processes)
 
-clients = []
 
-while len(clients) < int(sys.argv[2]):
-    clients.append(subprocess.Popen('python3 ' + filename, shell=True))
-
-while True:
-    for proc in clients:
-        if proc.poll() is not None:
-            proc.kill()
-            proc = subprocess.Popen('python3 ' + filename, shell=True)
-
-    time.sleep(1)
