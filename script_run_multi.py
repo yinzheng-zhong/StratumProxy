66c9bed05074e273a4dd6266d8d1d96c8fb8f1df
zxhaxdr <zxhaxdr@gmail.com>
2021-10-19 18:10:29 +0100

    10 parallel

--- script_run_multi.py
+++ script_run_multi.py
@@ -8,10 +8,10 @@ filename = 'main.py ' + arg
 
 clients = []
 
-while True:
-    while len(clients) < 10:
-        clients.append(subprocess.Popen('python3 ' + filename, shell=True))
+while len(clients) < 10:
+    clients.append(subprocess.Popen('python3 ' + filename, shell=True))
 
+while True:
     for proc in clients:
         if proc.poll() is None:
             proc = subprocess.Popen('python3 ' + filename, shell=True)
