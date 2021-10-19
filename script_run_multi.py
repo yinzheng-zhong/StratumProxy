3d87317f7d51fe24899dab6f6f4d8031f3aacdf7
zxhaxdr <zxhaxdr@gmail.com>
2021-10-19 20:08:17 +0100

    add wait()

--- script_run_multi.py
+++ script_run_multi.py
@@ -1,23 +1,34 @@
 import subprocess
 import time
 import sys
-import threading
 
 
-def manage():
-    arg = sys.argv[1]
+class ProcManager:
+    def __init__(self, filename, num_procs):
+        self.filename = filename
+        self.num_procs = num_procs
 
-    filename = 'main.py ' + arg
-    while True:
-        subprocess.Popen('python3 ' + filename, shell=True).wait()
+        self.proc_list = []
 
-        time.sleep(1)
+        self.maintain()
+
+    def add_procs(self):
+        while len(self.proc_list) < self.num_procs:
+            self.proc_list.append(subprocess.Popen('python3 ' + self.filename, shell=True).wait())
+
+    def remove_dead_procs(self):
+        self.proc_list = [proc for proc in self.proc_list if proc.poll()]
+
+    def maintain(self):
+        while True:
+            self.add_procs()
+
+            for proc in self.proc_list:
+                proc.wait()
 
 
-threads = []
-for i in range(int(sys.argv[2])):
-    threads.append(threading.Thread(target=manage))
+filename = 'main.py ' + sys.argv[1]
+num_processes = int(sys.argv[2])
+pm = ProcManager(filename, num_processes)
 
-for thread in threads:
-    thread.start()
-    thread.join()
+
