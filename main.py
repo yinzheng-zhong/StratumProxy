15cf0554a418a6cb4b6ee9cd28e61345d6027c70
zxhaxdr <zxhaxdr@gmail.com>
2021-10-30 14:38:23 +0100

    added gsr

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ class Server:
             _, profitability = self.api.get_most_profitable()
 
             if profitability > 0:
-                if profitability * 0.5 > self.setting.get_bid():
+                if profitability * 0.8 > self.setting.get_bid():
                     if self.prev_profitable != 1:
                         Logger.important('Profitable: ' + str(profitability))
                         self.prev_profitable = 1
