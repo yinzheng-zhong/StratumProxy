407c416f2b344d7acb5ea688dbfe85b4a4a7adb0
zxhaxdr <zxhaxdr@gmail.com>
2021-10-30 11:00:42 +0100

    fixed sha256 algo pool

--- src/Helper/utils.py
+++ src/Helper/utils.py
@@ -3,5 +3,7 @@ class Utils:
     def zerg_to_nice_algo_converter(zerg_algo):
         if zerg_algo == 'ethash':
             return 'daggerhashimoto'
+        elif zerg_algo == 'sha256':
+            return 'sha256asicboost'
         else:
             return zerg_algo
